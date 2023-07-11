78a2b08b6349e96cb78dcaa97a49600f817c61b9
Кирилл Руденко <slimpkowl@gmail.com>
2023-07-11 16:13:31 +0700

    infraaaa

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,9 +8,13 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = bool(strtobool(os.getenv('DEBUG', 'True')))
 
-DEBUG = bool(strtobool(os.getenv('DEBUG', 'False')))
+if DEBUG:
+    SECRET_KEY = 'default_value'
+else:
+    load_dotenv()
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
